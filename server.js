6e8f3d75fece46bc3cd49528cbd13248f60a854b
unknown <hsuntariq@gmail.com>
2023-03-26 16:03:21 +0500

    entry point

---
+++ server.js
@@ -0,0 +1,14 @@
+// require express
+const express = require("express");
+// require dotenv to use .env variables
+const dotenv = require("dotenv").config();
+// require colors for styling in the terminal
+const colors = require("colors");
+// initialize express to a variable
+const app = express();
+// all the api's hit the same end point
+app.use("/api/rest", require("./routes/testRotes"));
+// build a server
+app.listen(process.env.PORT, () =>
+  console.log(`Server started on port:${process.env.PORT.cyan}`)
+);
